065b09321dbcbfe4a1a8da75a0ab93abbac0ca78
jessica.poh@servian.com <jessica.poh@servian.com>
2022-11-28 03:53:56 +0000

    update dev

--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 dbt deps --profiles-dir .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt debug --target dev --profiles-dir .
-dbt debug --target prod --profiles-dir .
-dbt run --target prod --profiles-dir .
-dbt test --data --target dev --profiles-dir .
+dbt run --target dev --profiles-dir .
